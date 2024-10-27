efb35024d78b29e75c58b62a509e9879c6de3a4a
marshaltrinov <154526264+marshaltrinov@users.noreply.github.com>
2024-10-27 10:33:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ async function addSeries() {
     const seriesList = document.getElementById('seriesList');
 
     if (seriesInput.value.trim() !== "") {
-        const response = await fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(seriesInput.value)}&apikey=4c8f70dd`);
+        const response = await fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(seriesInput.value)}&apikey=4c8f70dd`);
         const data = await response.json();
 
         if (data.Response === "True") {
             const li = document.createElement('li');
-            li.innerHTML = `<strong>${data.Title}</strong> (${data.Year}) - ${data.Genre}`;
+            li.innerHTML = `
+                <strong>${data.Title}</strong> (${data.Year})<br>
+                <img src="${data.Poster}" alt="${data.Title} Poster" style="width:100px;"><br>
+                <em>${data.Genre}</em><br>
+                <p>${data.Plot}</p>
+                <p><strong>Rating:</strong> ${data.imdbRating} (${data.imdbVotes} votes)</p>
+            `;
             seriesList.appendChild(li);
             seriesInput.value = ""; // Clear input
         } else {
